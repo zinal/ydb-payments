5d1e396d8da7434968b824737f2d76fe2ad3e749
Maksim Zinal <mzinal@ru.ibm.com>
2023-04-28 12:54:08 +0300

    progress

--- scripts/ydb-payments.sql
+++ scripts/ydb-payments.sql
@@ -1,10 +1,25 @@
+-- ydb yql -f scripts/ydb-payments.sql
+
+--DROP TABLE `demo-payments/settings`;
+--DROP TABLE `demo-payments/account`;
+--DROP TABLE `demo-payments/saldo`;
+--DROP TABLE `demo-payments/transaction`;
+--DROP TABLE `demo-payments/oporder`;
+--DROP TABLE `demo-payments/inbox`;
+--COMMIT;
+
+CREATE TABLE `demo-payments/settings`(
+  sid Utf8 NOT NULL,
+  day Date,
+  PRIMARY KEY(sid)
+);
 
 CREATE TABLE `demo-payments/account`(
-    id String,
+    id String NOT NULL,
     num Utf8,
     name Utf8,
-    date_open Date,
-    date_close Date,
+    date_opened Date,
+    date_closed Date,
     PRIMARY KEY(id)
 ) WITH (
    AUTO_PARTITIONING_BY_SIZE = ENABLED,
@@ -15,13 +30,13 @@ CREATE TABLE `demo-payments/account`(
 );
 
 CREATE TABLE `demo-payments/saldo`(
-    acc_id String,
-    day Date,
-    num_trans Int32,
-    saldo Decimal,
-    turn_dt Decimal,
-    turn_kt Decimal,
-    PRIMARY KEY(acc_id, day)
+    day Date NOT NULL,
+    acc_id String NOT NULL,
+    num_trans UInt32,
+    saldo Decimal(22,9),
+    turn_dt Decimal(22,9),
+    turn_kt Decimal(22,9),
+    PRIMARY KEY(day, acc_id)
 ) WITH (
    AUTO_PARTITIONING_BY_SIZE = ENABLED,
    AUTO_PARTITIONING_BY_LOAD = ENABLED,
@@ -31,13 +46,13 @@ CREATE TABLE `demo-payments/saldo`(
 );
 
 CREATE TABLE `demo-payments/transaction`(
-    id String,
+    id String NOT NULL,
     day Date,
     acc_dt String,
-    num_dt Int32,
     acc_kt String,
-    num_kt Int32,
-    amount Decimal,
+    num_dt UInt32,
+    num_kt UInt32,
+    amount Decimal(22,9),
     reason Utf8,
     state Int32,
     PRIMARY KEY(id)
@@ -45,6 +60,36 @@ CREATE TABLE `demo-payments/transaction`(
    AUTO_PARTITIONING_BY_SIZE = ENABLED,
    AUTO_PARTITIONING_BY_LOAD = ENABLED,
    AUTO_PARTITIONING_PARTITION_SIZE_MB = 500,
+   AUTO_PARTITIONING_MIN_PARTITIONS_COUNT = 500,
+   AUTO_PARTITIONING_MAX_PARTITIONS_COUNT = 600
+);
+
+CREATE TABLE `demo-payments/oporder`(
+    acc_id String NOT NULL,
+    num_trans UInt32,
+    PRIMARY KEY(acc_id)
+) WITH (
+   AUTO_PARTITIONING_BY_SIZE = ENABLED,
+   AUTO_PARTITIONING_BY_LOAD = ENABLED,
+   AUTO_PARTITIONING_PARTITION_SIZE_MB = 100,
    AUTO_PARTITIONING_MIN_PARTITIONS_COUNT = 200,
    AUTO_PARTITIONING_MAX_PARTITIONS_COUNT = 250
 );
+
+CREATE TABLE `demo-payments/inbox`(
+    id String NOT NULL,
+    acc_dt String,
+    num_dt UInt32,
+    acc_kt String,
+    amount Decimal(22,9),
+    reason Utf8,
+    PRIMARY KEY(id)
+) WITH (
+   AUTO_PARTITIONING_BY_SIZE = ENABLED,
+   AUTO_PARTITIONING_BY_LOAD = ENABLED,
+   AUTO_PARTITIONING_PARTITION_SIZE_MB = 500,
+   AUTO_PARTITIONING_MIN_PARTITIONS_COUNT = 500,
+   AUTO_PARTITIONING_MAX_PARTITIONS_COUNT = 600
+);
+
+-- End Of File
